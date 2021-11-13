9a424a982d201004ea2a779a02d3ae34dc6e2069
JoJo-19 <89957410+JoJo-19@users.noreply.github.com>
2021-11-12 22:16:01 -0500

    So far so good, moon+star+universe
    No animation tho

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
+//set up
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -49,6 +50,39 @@ scene.add(lightHelper, gridHelper);
 
 //Orbit Control, listen to mouse and update
 const controls = new OrbitControls(camera, render.domElement);
+
+//outter space
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( {color: 0xffffff});
+  const star = new THREE.Mesh(geometry, material);
+  //random xyz
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+//200 random position stars
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+//adding texture to moon
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture
+  })
+);
+
+scene.add(moon);
+
 
 //recursive function
 function animate() {
